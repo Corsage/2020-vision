609533f21eac3f739fbdf618234650908622ecbc
Dhyan Pathak <dhyan.pathak@gmail.com>
2020-11-21 02:20:44 -0500

    node js socket server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const http = require('http');
 const bodyParser = require('body-parser'); //parse incoming request as json
 const morgan = require('morgan'); //logging framework for requests
 const app = express();
@@ -11,7 +10,30 @@ app.use(cors());
 app.use(bodyParser.json({ type: '*/*' }));
 router(app);
 
-const port = process.env.PORT || 8080;
-const server = http.createServer(app);
-server.listen(port);
-console.log('server listening on port '+port);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+const cvNamespace = io.of('/cv');
+const webNamespace = io.of('/web');
+
+cvNamespace.on("connection", (socket) => {
+    console.log("CV client connected");
+    socket.on('ocv_image', (data) => {
+        webNamespace.emit('incoming-cv', data);
+    });
+    socket.on('disconnect', () => {
+        console.log('CV client disconnected');
+    });
+});
+
+webNamespace.on("connection", (socket) => {
+    console.log("Web client connected");
+    socket.on('disconnect', () => {
+        console.log('Web client disconnected');
+    });
+});
+
+const port = process.env.PORT || 6969;
+http.listen(port, () => {
+    console.log('server listening on port '+port);
+});
