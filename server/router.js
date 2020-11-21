467e0acb2a8ce7c238305289cfd0ffa2e362acb7
Sanjot Boyal <sboyal2@uwo.ca>
2020-11-21 05:50:35 -0500

    Implementing python executable call.

--- server/router.js
+++ server/router.js
@@ -1,9 +1,33 @@
 const Dashboard = require('./controllers/dashboard');
+const { exec } = require('child_process');
 
 module.exports = function(app) {
     app.get('/', function(req, res) {
         res.send('hello world');
     });
+    
+    app.get('/testFunction', function(req, res) {
+        console.log(__filename);
+        
+        exec('python /home/ec2-user/environment/2020-vision/opencv/run.py --prototxt mobilenet_ssd/MobileNetSSD_deploy.prototxt --model mobilenet_ssd/MobileNetSSD_deploy.caffemodel --input /home/ec2-user/environment/2020-vision/opencv/videos/example_01.mp4', (err, stdout, stderr) => {
+            if (err) {
+                // node couldn't execute the command
+                console.log("failed");
+                console.log(err);
+                res.send("failed");
+                return;
+
+            }
+            res.send("passed");
+            
+            // the *entire* stdout and stderr (buffered)
+            console.log(`stdout: ${stdout}`);
+            console.log(`stderr: ${stderr}`);
+        });
+    });
+    
     app.get('/dashboard', Dashboard.onload);
     app.post('/alert', Dashboard.alert);
+    
+    
 }
--- server/server.js
+++ server/server.js
@@ -11,15 +11,19 @@ const cors = require('cors');
 const socket = require('./socket');
 
 const app = express();
+
+const morgan = require('morgan'); //logging framework for requests
+const router = require('./router');
 const server = http.createServer(app);
 
 // Socket setup.
 socket(server);
 
 // JSON API setup.
+app.use(morgan('combined'));
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Start the server.
 server.listen(8080, () => {
