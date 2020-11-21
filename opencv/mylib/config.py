c51fc5ed555db7e3c408658ef165e6f7917cb571
Jay Chowdhary <jay.chowdhary@ryerson.ca>
2020-11-21 00:38:57 -0500

    Added base opencv project.

---
+++ opencv/mylib/config.py
@@ -0,0 +1,23 @@
+#===============================================================================
+""" Optional features config. """
+#===============================================================================
+# Enter mail below to receive real-time email alerts
+# e.g., 'email@gmail.com'
+MAIL = ''
+# Enter the ip camera url (e.g., url = 'http://191.138.0.100:8040/video')
+url = 'http://200.52.68.182:8081/cgi-bin/mjpeg?stream=1&resolution=2048x2048&quality=1&page=1605931124869&Language=0'
+
+# ON/OFF for mail feature. Enter True to turn on the email alert feature.
+ALERT = False
+# Set max. people inside limit. Optimise number below: 10, 50, 100, etc.
+Threshold = 10
+# Threading ON/OFF
+Thread = False
+# Simple log to log the counting data
+Log = False
+# Auto run/Schedule the software to run at your desired time
+Scheduler = False
+# Auto stop the software after certain a time/hours
+Timer = False
+#===============================================================================
+#===============================================================================
