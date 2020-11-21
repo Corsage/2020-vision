e31851a6dde593099d7926905f931c3d1c66bdb2
Yetix33 <apate388@uwo.ca>
2020-11-21 02:17:11 -0500

    changes to default theme, create small stat metrics

--- front/my-app/src/components/DashboardOverview.js
+++ front/my-app/src/components/DashboardOverview.js
@@ -1,6 +1,7 @@
-import { Card, Container, Grid, makeStyles } from '@material-ui/core'
+import { Button, Card, Container, Grid, makeStyles } from '@material-ui/core'
 import React from 'react'
 import { Bar } from 'react-chartjs-2';
+import SmallStat from './SmallStat';
 
 const useStyles = makeStyles((theme) => ({
     app: {
@@ -9,20 +10,20 @@ const useStyles = makeStyles((theme) => ({
     barCard: {
         width: "100%",
         height: '30rem',
-        padding:'2rem'
+        padding: '2rem'
     },
     barChart: {
-        width:'100%',
-        maxWidth:'100%',
+        width: '100%',
+        maxWidth: '100%',
     }
 }))
 
 const DashboardOverview = () => {
     var classes = useStyles()
-     var chartLabels = [];
-     var chartData = []
-     const chartReference = React.createRef();
-    for(var x = 1; x<25; x++ ){
+    var chartLabels = [];
+    var chartData = []
+    const chartReference = React.createRef();
+    for (var x = 1; x < 25; x++) {
         const date = new Date(2020, 11, 21, x, 0, 0, 0)
         chartLabels.push(date.getHours())
         chartData.push(Math.floor(Math.random() * 100))
@@ -36,27 +37,45 @@ const DashboardOverview = () => {
     const data = {
         labels: chartLabels,
         datasets: [
-          {
-            label: 'My First dataset',
-            backgroundColor: 'rgba(255,99,132,0.2)',
-            borderColor: 'rgba(255,99,132,1)',
-            borderWidth: 1,
-            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-            hoverBorderColor: 'rgba(255,99,132,1)',
-            data: chartData
-          }
+            {
+                label: 'My First dataset',
+                backgroundColor: 'rgba(255,99,132,0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                borderWidth: 1,
+                hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                hoverBorderColor: 'rgba(255,99,132,1)',
+                data: chartData
+            }
         ]
-      };
+    };
 
     return (
-            <Grid container direction='row' justify='flex-start' style={{ paddingLeft: '4rem' , paddingRight:'1rem'}} >
-                <Card className={classes.barCard}>
+        <Grid container direction='column' style={{ paddingLeft: '4rem', paddingRight: '1rem', paddingTop:'2rem' }}>
+            <Grid container direction='row' justify='flex-start' style={{marginBottom:'1rem'}}>
+                <Grid container direction='column' md={3} style={{padding:'0 0.5rem'}}>
+                    <SmallStat />
+                </Grid>
+                <Grid container direction='column' md={3} style={{padding:'0 0.5rem'}}>
+                    <SmallStat />
+                </Grid>
+                <Grid container direction='column' md={3} style={{padding:'0 0.5rem'}}>
+                    <SmallStat />
+                </Grid>
+                <Grid container direction='column' md={3} style={{padding:'0 0.5rem'}}>
+                    <SmallStat />
+                </Grid>
+                
+            </Grid>
+            <Grid container direction='row' justify='flex-start' >
+                <Card className={classes.barCard} raised>
                     <Bar ref={chartReference} data={data}
                         width={'100%'}
-                         options={{ maintainAspectRatio: false }}
+                        options={{ maintainAspectRatio: false }}
                     />
                 </Card>
             </Grid>
+        </Grid>
+
     )
 }
 
--- front/my-app/src/components/SmallStat.js
+++ front/my-app/src/components/SmallStat.js
@@ -1,3 +1,93 @@
+import { Card, CardContent, Grid, Typography } from '@material-ui/core'
 import React from 'react'
+import { Line } from 'react-chartjs-2';
 
 
+const SmallStat = () => {
+    var chartLabels = [];
+    var chartData = []
+    const chartReference = React.createRef();
+    for (var x = 1; x < 25; x++) {
+        const date = new Date(2020, 11, 21, x, 0, 0, 0)
+        chartLabels.push(date.getHours())
+        chartData.push(Math.floor(Math.random() * 100))
+    }
+    console.log(chartLabels)
+    // const data = {
+    //     labels: chartLabels,
+    //     datasets: {data: chartData }
+    // }
+
+    const data = {
+        labels: chartLabels,
+        datasets: [
+            {
+                label: 'My First dataset',
+                backgroundColor: 'rgba(255,99,132,0.2)',
+                borderColor: 'rgba(255,99,132,1)',
+                data: chartData
+            }
+        ]
+    };
+
+
+    const chartOptions = {
+        maintainAspectRatio: false,
+        legend: {
+            display: false
+        },
+        tooltips: {
+            enabled: false,
+            custom: false
+        },
+        elements: {
+            point: {
+                radius: 0
+            },
+            line: {
+                tension: 0.33
+            }
+        },
+        scales: {
+            xAxes: [
+                {
+                    gridLines: false,
+                    ticks: {
+                        display: false
+                    }
+                }
+            ],
+            yAxes: [
+                {
+                    gridLines: false,
+                    scaleLabel: false,
+                    ticks: {
+                        display: false,
+                    }
+                }
+            ]
+        }
+
+    };
+
+    return (
+        <Card style={{ width: '100%', height: '10rem' }} raised>
+            <Grid container direction='column' alignItems='center' justify='center' style={{height:'100%', position:'relative'}}>
+                <Typography variant={'h4'}>TOTAL PEOPLE</Typography>
+                <Typography variant={'h3'}>40</Typography>
+                <div style={{bottom:0, position:'absolute', width:'100%'}}>
+                    <Line
+                        width={'100%'}
+                        height={50}
+                        ref={chartReference}
+                        data={data}
+                        options={chartOptions}
+                    />
+                </div>
+            </Grid>
+
+        </Card>
+    )
+}
+
+export default SmallStat
